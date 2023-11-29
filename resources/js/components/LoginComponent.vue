bf2a8b254332f63c49184c9a3f51f693ae01ece8
gplisich <gplisich@somil.com.uy>
2023-11-29 10:36:20 -0300

    Primer Commit

---
+++ resources/js/components/LoginComponent.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title mb-4">Sign In</h5>
+
+                <el-form ref="form" :model="form" :rules="rulesForm">
+                    
+                    
+                    <el-form-item  label="Email" prop="email">
+                        <el-input v-model="form.email" prop="email" ref="email" @change="focusInput('password')"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Password" prop="password">
+                        <el-input show-password v-model="form.password" prop="password" ref="password"></el-input>
+                    </el-form-item>
+                    <div v-show="showError" class="error-login">
+                        {{ this.validationErrors }}
+                    </div>
+                        
+                    <div class="d-grid gap-2">
+                        <el-button 
+                            :disabled="isSubmitting"
+                            @click="loginAction('form')"
+                            type="primary"
+                            ref="sumbit">Login
+                        </el-button>
+                        <p class="text-center">No tienes una cuenta? 
+                            <el-link type="primary" href="/register">Registrate aqui</el-link>
+                        </p>
+                    </div>
+
+
+                </el-form>
+            
+            </div>
+        </div>
+    </div>
+ </template>
+   
+<script>
+   
+export default {
+    data() {
+        return {
+            showError:false,
+            form:{
+                email:'',
+                password:'',
+            },
+            rulesForm:{
+                email:[{required: true, message: 'El email no puede estar vacio', trigger: 'blur'}],
+                password:[{required: true, message: 'La password no puede ser vacia', trigger: 'blur'}],
+            },
+            
+            validationErrors:'',
+            isSubmitting:false,
+        };
+    },
+    mounted(){
+        this.focusInput('email');
+    },
+    methods: {
+
+        focusInput(input) {
+            this.$refs[input].focus() 
+        },
+        
+        loginAction(form){
+            
+            this.$refs[form].validate((valid) => {
+                
+                if(valid){
+                    var form = this.form;
+                    this.isSubmitting = true;
+                    axios.post('/login/loguearse', form)
+                    .then(response => {
+                        const mensaje = response.data.message;
+                        if(mensaje!=''){
+                            this.showError=true;
+                            this.validationErrors=response.data.message;
+                            this.isSubmitting = false;
+                        }else{
+                            const redirectTo = response.data.redirectTo;
+                            localStorage.setItem('token', response.data.token)
+                            if(redirectTo!=null){
+                                window.location.href='/'+redirectTo;
+                            }else{
+                                window.location.href='/';
+                            }
+                        }
+                        
+                        
+                    })
+                }
+            });
+        }
+    },
+};
+</script>
+<style>
+    .error-login{
+       color: red;
+       font-weight: bold; 
+       text-align: center;
+    }
+</style>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    if (session()->exists('userLogin')) {
+        return redirect('home');
+    } else{
+        return view('auth.login');	
+    }
+});
+
+Route::get('/home', 'HomeController@index');
+Route::get('/login', 'Auth\LoginController@show')->name('login');
+Route::get('/register', 'Auth\RegisterController@show');
+Route::post('/login/logout', 'Auth\LoginController@logout');
+Route::post('/login/loguearse', 'Auth\LoginController@loguearse');
+// Route::resource('/login', 'Auth\LoginController');
